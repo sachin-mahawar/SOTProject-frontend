23cd5443b75c0d330820a932274c6a2f94a825b8
Sachin Mahawar <sachin.a.mahawar@accenture.com>
2025-01-27 11:21:35 +0900

    resolved some commit errors

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import './App.css'
-import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import SearchTrains from "./pages/user/SearchTrains"
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import SearchTrains from "./pages/user/SearchTrains";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import PassangerDetail from "./pages/user/PassengerDetail";
 
 function App() {
-
   return (
     <Routes>
-      <Route path='/' element={<SearchTrains/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/signup' element={<Signup/>}/>
-
+      <Route path="/" element={<SearchTrains />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="passenger-detail" element={<PassangerDetail />}></Route>
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
